a231c584a3e4b0e7009b67ecafaca0286af06c10
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2025-12-29 13:08:21 +0700

    feat: transformed the microservice app into hybrid app due to render limitations

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,11 +5,18 @@ import { MicroserviceOptions } from '@nestjs/microservices';
 import { buildRmqServerOptions } from '@app/common';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AuthModule,
+  const app = await NestFactory.create(AuthModule);
+
+  app.connectMicroservice<MicroserviceOptions>(
     buildRmqServerOptions(process.env.RABBIT_MQ_AUTH_QUEUE ?? 'auth.queue'),
   );
 
-  await app.listen();
+  await app.startAllMicroservices();
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`Auth service running with RMQ + HTTP on port ${port}`);
 }
+
 void bootstrap();
